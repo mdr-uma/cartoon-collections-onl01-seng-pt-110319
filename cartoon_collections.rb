d717911a6a8d352fc963fc5f27306258e140ac9b
Uma Manandhar <um_2003_8@hotmail.com>
2020-10-12 20:03:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,10 @@ def long_planeteer_calls(calls_long)# code an argument here
   calls_long.any? { |e| e.length > 4  }
 end
 
-def find_the_cheese(cheese)# code an argument here
+def find_the_cheese(cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.find do |cheese|
+    cheese_types.include? (cheese)
+  end
 end
