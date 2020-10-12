a1bad9de4fa7cf6eecd1f028a20b86339c66258a
Uma Manandhar <um_2003_8@hotmail.com>
2020-10-12 19:56:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dravers)# code an argument here
+  dravers.each_with_index do |draver, i|
+    puts "#{i + 1} #{draver}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)# code an argument here
+  veggies.map { |e| e.capitalize + "!"  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)# code an argument here
+  calls_long.any? { |e| e.length > 4  }
 end
 
 def find_the_cheese# code an argument here
